02ab7fb40ef6be52073ddc4f38f7ad3a1f6619ee
pracniscate <lorena.giddens@outlook.com>
2019-01-15 20:13:57 -0500

    add data files

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Boar Bristle Paddle Hair Brush",
+    img: "img/product-1.jpg",
+    price: 10,
+    company: "Janrely",
+    description: "Join the most popular color recent years.The rubber material is nice to touch and offers a warmer feel than plastic. This grips help to reduce vibration, enabling a more comfortable and secure grip. It is pretty rugged and design for occidental palm.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Blowout Nano Thermic Ceramic & Ionic Round Barrel Hair Brush",
+    img: "img/product-2.jpg",
+    price: 15,
+    company: "SUPRENT",
+    description: "Human scalp naturally produces oil called sebum which acts as a protector for hair. The boar bristles help to spread this sebum around to lubricate each of your hair strands which reduces frizz. For Smooth Finishing and Quick Drying. Enhances Shine with Natural Boar Bristles. Large round shape creates volume and curl shine with natural boar bristles. Anti-static, anti-bacterial. Highly heat resistant;",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Cushion Brush Nylon Bristles, 7-Row",
+    img: "img/product-3.jpg",
+    price: 9,
+    company: "Denman",
+    description: "Smooth nylon pins in an anti-static, natural rubber pad. Brush Width 35 mm",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Wet Brush Pro Paddle Hair Brush, Blackout",
+    img: "img/product-4.jpg",
+    price: 7,
+    company: "Wet Brush",
+    description: "Detangles hair with ease and without pain",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Boar Bristle Hair Brush",
+    img: "img/product-5.jpg",
+    price: 19,
+    company: "Beauty by Earth",
+    description: "When you use our eco friendly, Bamboo Boar Brush(see instructions below) you may find heavy styling products become unnecessary. As your hair becomes healthier, softer, and conditioned with regular brushing, you’ll no longer need to use leave-in conditioners or hair expensive, oily hair serums.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Dtang Hair Brush 2 Pack Metallic Pink & Purple Best Detangler Hair Brush for Wet & Dry Hair...",
+    img: "img/product-6.jpg",
+    price: 8,
+    company: "Dtang Brushes",
+    description: "Say Goodbye to Tangled Hair – Follow new Hair Trends Now, you don’t have to fret at all before heading to parties as this detangler hair brush is going to help you get your hair styled in any way you desire. Undoubtedly, hairstyling is challenging when you have tangled hair that don’t get detangled easily. This detangler hair will smoothen your hair so that you can experiment with your hair and keep up with the ever-changing hairstyling trends.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "ArtNaturals Detangling Hair Brush Set",
+    img: "img/product-7.jpg",
+    price: 13,
+    company: "ArtNaturals",
+    description: "Ordinary brushes work against the hair ripping through the knots. Our cone shaped bristles separate the hair sideways, gently unraveling the toughest tangles pain free.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Hair Brush CHOSIN Natural Boar Bristle",
+    img: "img/product-8.jpg",
+    price: 18,
+    company: "CHOSIN",
+    description: "Nylon pins gently massage scalp, stimulate blood circulation, relax scalp's pressure. Boar bristle pins spread natural hair oil to the whole hair like conditioner, create smooth, silky and shiny hair, moisturize hair, reduce hair loss and frizz, prevent splitting, decrease static",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
