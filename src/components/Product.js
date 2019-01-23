4e9ebfba59f49d3654bdbbcd2a9a12e94dfd2c74
pracniscate <lorena.giddens@outlook.com>
2019-01-22 23:36:10 -0500

    show product title & price

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,31 +10,43 @@ export default class Product extends Component {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
-        <div className="img-container p-5" onClick={() => console.log('clicked on image container.')
-        }>
-        <Link to="/details">
-        {/* get the img */}
-        <img src={img} alt="product image" className="card-img-top" />
-        </Link>
+          <div className="img-container p-5" 
+            onClick={() => console.log('clicked on image container.')
+            }>
 
-        {/* conditional rendering */}
-        <button 
-          className="cart-btn" 
-          disabled={inCart ? true: false} 
-          onClick={() => {
-            console.log('added to the cart');
-          }} 
-        >
-          {inCart ? (
-            <p className="text-capitalize mb-0" disabled>
-              {" "}
-              in cart
+            <Link to="/details">
+              {/* get the img */}
+              <img src={img} alt="product image" className="card-img-top" />
+            </Link>
+
+            {/* conditional rendering */}
+            <button 
+              className="cart-btn" 
+              disabled={inCart ? true: false} 
+              onClick={() => {
+                console.log('added to the cart');
+              }} 
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  {" "}
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+          {/* card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">
+              {title}
             </p>
-          ) : (
-            <i className="fas fa-cart-plus" />
-          )}
-        </button>
-        </div>
+            <h5 className="text-green font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
         </div>
       </ProductWrapper>
     )
