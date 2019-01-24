74897aae6cd4177688662365711821b8dabcea3f
pracniscate <lorena.giddens@outlook.com>
2019-01-23 21:42:42 -0500

    style the product containers & add to cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -53,6 +53,53 @@ export default class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.section`
-
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(201, 171, 186, 0.88);
+      box-shadow: 2px 2px 5px 0px rgba(201, 171, 186, 0.88);
+    }
+    .card-footer {
+      background: rgba(234, 138, 186, 0.88);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+  .card-img-top {
+    transition: all 1s linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background: var(--blueBell);
+    border: none;
+    color: white;
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 1s linear;
+  }
+  .img-container:hover .cart-btn {
+    transform: translate(0, 0);
+  }
+  .cart-btn:hover {
+    color: black;
+    cursor: pointer;
+  }
 `
