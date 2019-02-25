f94555aa538171f9200feadde70a90a3849982c6
pracniscate <lorena.giddens@outlook.com>
2019-02-24 20:10:05 -0500

    set up conditional cart rendering

--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailsProduct,
-    cart: [],
+    cart: storeProducts,
     modalOpen: false,
     modalProduct: detailsProduct,
     cartSubTotal: 0,
