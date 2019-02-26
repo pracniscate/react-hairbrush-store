fa9e3642794a576ad80a8be2252f6f90c515fbeb
pracniscate <lorena.giddens@outlook.com>
2019-02-26 08:18:53 -0500

    display images of products added to cart in /cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-export default function CartItem() {
-  return <div>hello from CartItem</div>;
+export default function CartItem({  item, value }) {
+  const  {  id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img 
+          src={img} 
+          style={{  width: '5rem', height: '5rem' }} className="img-fluid" 
+          alt="product" 
+        />
+      </div>
+    </div>
+  )
 }
