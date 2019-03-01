21eda572b3dd5ace4da06193eb913f834a10102b
pracniscate <lorena.giddens@outlook.com>
2019-03-01 07:47:30 -0500

    remove single item from cart

--- src/context.js
+++ src/context.js
@@ -88,7 +88,26 @@ class ProductProvider extends Component {
   };
 
   removeItem = (id) => {
-    console.log('item removed');
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    // find item that doesn't match id
+    tempCart = tempCart.filter(item => item.id !== id);
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      }
+    }, () => {
+      this.addTotals();
+    })
   };
 
   clearCart = () => {
